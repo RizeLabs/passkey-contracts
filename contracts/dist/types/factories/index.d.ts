00997d4725c326c551a2c043773bc83e5fb8dbae
nlok5923 <nlokhande5923@gmail.com>
2023-06-16 12:29:17 +0530

    bundling contracts

---
+++ contracts/dist/types/factories/index.d.ts
@@ -0,0 +1,4 @@
+export { IPasskeyManager__factory } from "./IPasskeyManager__factory";
+export { PassKeyManagerFactory__factory } from "./PassKeyManagerFactory__factory";
+export { PasskeyManager__factory } from "./PasskeyManager__factory";
+export { Secp256r1__factory } from "./Secp256r1__factory";
---
+++ contracts/dist/types/index.d.ts
@@ -0,0 +1,9 @@
+export type { IPasskeyManager } from "./IPasskeyManager";
+export type { PassKeyManagerFactory } from "./PassKeyManagerFactory";
+export type { PasskeyManager } from "./PasskeyManager";
+export type { Secp256r1 } from "./Secp256r1";
+export * as factories from "./factories";
+export { IPasskeyManager__factory } from "./factories/IPasskeyManager__factory";
+export { PassKeyManagerFactory__factory } from "./factories/PassKeyManagerFactory__factory";
+export { PasskeyManager__factory } from "./factories/PasskeyManager__factory";
+export { Secp256r1__factory } from "./factories/Secp256r1__factory";
